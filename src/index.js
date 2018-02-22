747de9a96a53f844cc0c0b266ff2df24d3aaa26b
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2018-02-22 10:15:16 -0300

    add redux

--- src/index.js
+++ src/index.js
@@ -8,22 +8,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import PhotoForm from './pages/PhotoForm/PhotoForm';
 import PhotoList from './pages/PhotoList/PhotoList';
+// tudo precisa estar dentro dele
+import { Provider } from 'react-redux';
+import store from './store';
 
 // BrowserRouter can have only one element, that's why the 
 // div is necessary
 // https://medium.com/@AkyunaAkish/understanding-react-router-4-df73a66d96c4
 // precisa do Switch caso contrário renderizará mais de um compo
+
+// é no Provider que definimos a store
 ReactDOM.render((
-    <BrowserRouter>
-        <div className="container">
-            <Switch>
-                <Route exact path="/form" component={PhotoForm}/>
-                <Route exact path="/form/:id" component={PhotoForm}/>
-                <Route exact path="/" component={PhotoList}/>
-                <Redirect from="*" to="/" />
-            </Switch>
-       </div>
-    </BrowserRouter>
+    <Provider store={ store }>
+        <BrowserRouter>
+            <div className="container">
+                <Switch>
+                    <Route exact path="/form" component={PhotoForm}/>
+                    <Route exact path="/form/:id" component={PhotoForm}/>
+                    <Route exact path="/" component={PhotoList}/>
+                    <Redirect from="*" to="/" />
+                </Switch>
+        </div>
+        </BrowserRouter>
+    </Provider>
   ), document.getElementById('root'))
 
 registerServiceWorker();
