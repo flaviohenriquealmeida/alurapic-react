825429565db1d31ad8c156a64d5c5e45ac36c418
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2018-02-28 15:18:00 -0300

    update

--- src/shared/Button/Button.js
+++ src/shared/Button/Button.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
+/* You have to convert a functional component to a class if you need 
+lifecycle methods, refs, or want to optimize performance using 
+shouldComponentUpdate/PureComponent.
+*/
 export const Button = props => {
 
     const clickHandler = () => 
